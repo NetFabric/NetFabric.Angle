252a3223f8260b9aecde871609672e56e4c85721
Antao Almada <antao.almada@gmail.com>
2016-04-25 11:34:08 +0100

    Added Angle serialization unit tests
    Had to add a .NET 3.5 unit tests projects

---
+++ NetFabric.Angle.Net35.UnitTests/AngleTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+namespace NetFabric.Net35.UnitTests
+{
+    [TestClass]
+    public class AngleTests
+    {
+
+        [TestMethod]
+        public void IsSerializationDefinedCorrectly()
+        {
+            var angle = Angle.FromDegrees(-45.0);
+            var formatter = new BinaryFormatter();
+            object result;
+            using (var stream = new MemoryStream())
+            {
+                formatter.Serialize(stream, angle);
+                stream.Seek(0, SeekOrigin.Begin);
+                result = formatter.Deserialize(stream);
+            }
+            Assert.AreEqual(angle, result);
+        }
+
+    }
+}

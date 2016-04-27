e3569cd11e6b2ca550acdc5f6243270c7bc2fef7
Antao Almada <antao.almada@gmail.com>
2016-04-27 22:31:54 +0100

    Added a Unity-specific project with a custom drawer for Angle

---
+++ NetFabric.Angle.Unity/AngleEditor.cs
@@ -0,0 +1,36 @@
+﻿using Supyrb;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+namespace NetFabric
+{
+    [CustomPropertyDrawer(typeof(Angle))]
+    public class AngleEditor
+        : PropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            EditorGUI.BeginProperty(position, label, property);
+
+            position = EditorGUI.PrefixLabel(position, GUIUtility.GetControlID(FocusType.Passive), label);
+
+            var min = Angle.Zero;
+            var max = Angle.Full;
+
+            var range = property.GetCustomAttributes<AngleRangeAttribute>(true).FirstOrDefault();
+            if(range != null)
+            {
+                min = range.Min;
+                max = range.Max;
+            }
+
+            var angle = property.GetValue<Angle>();
+            var newAngle = Angle.FromDegrees(EditorGUI.Slider(position, (float)angle.ToDegrees(), (float)min.ToDegrees(), (float)max.ToDegrees()));
+            if (newAngle != angle)
+                property.SetValue<Angle>(newAngle);
+
+            EditorGUI.EndProperty();
+        }
+    }
+}
---
+++ NetFabric.Angle.Unity/AngleRangeAttribute.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using UnityEngine;
+
+namespace NetFabric
+{
+    public abstract class AngleRangeAttribute
+        : PropertyAttribute
+    {
+        public abstract Angle Min { get; }
+
+        public abstract Angle Max { get; }
+    }
+
+    public class AngleRadiansRangeAttribute 
+        : AngleRangeAttribute
+    {
+        readonly double min;
+        readonly double max;
+
+        public AngleRadiansRangeAttribute(double min, double max)
+        {
+            if (min > max)
+                throw new ArgumentException("min must be less or equal to max.");
+
+            this.min = min;
+            this.max = max;
+        }
+
+        public override Angle Min { get { return Angle.FromRadians(min); } }
+
+        public override Angle Max { get { return Angle.FromRadians(max); } }
+    }
+
+    public class AngleDegreesRangeAttribute
+        : AngleRangeAttribute
+    {
+        readonly double min;
+        readonly double max;
+
+        public AngleDegreesRangeAttribute(double min, double max)
+        {
+            if (min > max)
+                throw new ArgumentException("min must be less or equal to max.");
+
+            this.min = min;
+            this.max = max;
+        }
+
+        public override Angle Min { get { return Angle.FromDegrees(min); } }
+
+        public override Angle Max { get { return Angle.FromDegrees(max); } }
+    }
+
+    public class AngleGradiansRangeAttribute
+        : AngleRangeAttribute
+    {
+        readonly double min;
+        readonly double max;
+
+        public AngleGradiansRangeAttribute(double min, double max)
+        {
+            if (min > max)
+                throw new ArgumentException("min must be less or equal to max.");
+
+            this.min = min;
+            this.max = max;
+        }
+
+        public override Angle Min { get { return Angle.FromGradians(min); } }
+
+        public override Angle Max { get { return Angle.FromGradians(max); } }
+    }
+
+}

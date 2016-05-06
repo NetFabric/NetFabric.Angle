efacdc2ceeeaf0a69c005bd9540092bfd28057bf
Antao Almada <antao.almada@gmail.com>
2016-05-06 22:51:58 +0100

    Added .VisualStudio project to add a debugger visualizer

---
+++ NetFabric.Angle.VisualStudio/AngleDebuggerVisualizer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.DebuggerVisualizers;
+
+namespace NetFabric
+{
+    public class AngleDebuggerVisualizer
+        : DialogDebuggerVisualizer
+    {
+        protected override void Show(IDialogVisualizerService windowService, IVisualizerObjectProvider objectProvider)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

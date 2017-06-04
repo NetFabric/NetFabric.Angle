88a469d1c848c2d1d27e9b0cac61622ac22d0f48
Antao Almada <antao.almada@gmail.com>
2017-06-04 23:26:19 +0100

    Added tests for tuples

--- NetFabric.Angle.UnitTests/AngleTest.cs
+++ NetFabric.Angle.UnitTests/AngleTest.cs
@@ -98,6 +98,26 @@ namespace NetFabric.UnitTests
         }
 
         [Test]
+        public void ToTupleDegreesMinutesConvertsFromRadiansCorrectly()
+        {
+
+            var angle = Angle.FromRadians(0.0);
+            (int degrees, double minutes) = angle.ToDegreesMinutes();
+            Assert.That(degrees, Is.Zero);
+            Assert.That(minutes, Is.Zero.Within(0.000001));
+
+            angle = Angle.FromRadians(0.706858347);
+            (int degrees1, double minutes1) = angle.ToDegreesMinutes();
+            Assert.That(degrees1, Is.EqualTo(40));
+            Assert.That(minutes1, Is.EqualTo(30.0).Within(0.000001));
+
+            angle = Angle.FromRadians(-0.706858347);
+            (int degrees2, double minutes2) = angle.ToDegreesMinutes();
+            Assert.That(degrees2, Is.EqualTo(-40));
+            Assert.That(minutes2, Is.EqualTo(30.0).Within(0.000001));
+        }
+
+        [Test]
         public void FromDegreesMinutesSecondsConvertsToRadiansCorrectly()
         {
             var angle = Angle.FromDegrees(0, 0, 0.0);
@@ -131,6 +151,29 @@ namespace NetFabric.UnitTests
             Assert.That(degrees, Is.EqualTo(-40));
             Assert.That(minutes, Is.EqualTo(30));
             Assert.That(seconds, Is.EqualTo(30.0).Within(0.001));
+        }
+
+        [Test]
+        public void ToTupleDegreesMinutesSecondsConvertsFromRadiansCorrectly()
+        {
+
+            var angle = Angle.FromRadians(0.0);
+            (int degrees, int minutes, double seconds) = angle.ToDegreesMinutesSeconds();
+            Assert.That(degrees, Is.Zero);
+            Assert.That(minutes, Is.Zero);
+            Assert.That(seconds, Is.Zero.Within(0.001));
+
+            angle = Angle.FromRadians(0.707003791);
+            (int degrees1, int minutes1, double seconds1) = angle.ToDegreesMinutesSeconds();
+            Assert.That(degrees1, Is.EqualTo(40));
+            Assert.That(minutes1, Is.EqualTo(30));
+            Assert.That(seconds1, Is.EqualTo(30.0).Within(0.001));
+
+            angle = Angle.FromRadians(-0.707003791);
+            (int degrees2, int minutes2, double seconds2) = angle.ToDegreesMinutesSeconds();
+            Assert.That(degrees2, Is.EqualTo(-40));
+            Assert.That(minutes2, Is.EqualTo(30));
+            Assert.That(seconds2, Is.EqualTo(30.0).Within(0.001));
         }
 
         [Test]

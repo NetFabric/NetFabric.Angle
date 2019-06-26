2e06d1a996aff84590cd8caee49f209e5fd4d7e7
Antao Almada <antao.almada@gmail.com>
2019-06-26 22:22:47 +0100

    Add trigonometry operations

---
+++ NetFabric.Angle/Trigonometry.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace NetFabric
+{
+    public static partial class Angle
+    {
+        /// <summary>
+        /// Return the sine of the specified angle.
+        /// </summary>
+        /// <param name="angle">An angle.</param>
+        /// <returns>The sine of the specified angle. If angle is equal to NaN, NegativeInfinity, or PositiveInfinity, this method returns NaN.</returns>
+        public static double Sin(AngleRadians angle) =>
+            Math.Sin(angle.Radians);
+    }
+}
